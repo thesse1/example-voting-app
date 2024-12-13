e22acac19f9eb3f3f3fd46eebd739fd5738c38c1
Thomas Hesse <hesse.thomas@gmx.de>
2024-12-13 11:28:58 +0100

    Debugging

--- result/server.js
+++ result/server.js
@@ -87,5 +87,6 @@ app.get('/', function (req, res) {
 
 server.listen(port, function () {
   var port = server.address().port;
+  console.log('Debugging...');
   console.log('App running on port ' + port);
 });
